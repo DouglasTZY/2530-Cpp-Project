1f4b9353a17aee2c16984b81d7113c502518cf05
Douglas Tan <douglastty3012@gmail.com>
2025-12-18 13:12:40 +0800

    OOP menu ok

--- main.cpp
+++ main.cpp
@@ -3,8 +3,103 @@
 #include <cstring> 
 using namespace std; 
 
-int main() {
+class User {
+	protected: 
+		char username[30]; 
+		char password[30]; 
+	
+	public: 
+		User() {
+			strcpy(username, ""); 
+			strcpy(password, ""); 
+		} 
+
+		virtual ~User() {
+			// destructor 
+		} 
+
+		virtual void menu() = 0; // pure virtual 
+}; 
+
+class Staff : public User {
+	public: 
+		Staff() {
+			// constructor 
+		} 
+
+		~Staff() {
+			// destructor 
+		} 
+
+		void menu() {
+			int choice; 
+
+			do {
+            cout << "\n--- Staff Menu ---\n";
+            cout << "1. Add Product\n";
+            cout << "2. Display Products\n";
+            cout << "0. Logout\n";
+            cout << "Enter choice: ";
+            cin >> choice;
+
+            switch(choice) {
+                case 1:
+                    cout << "Add Product (coming soon)\n";
+                    break;
+                case 2:
+                    cout << "Display Products (coming soon)\n";
+                    break;
+                case 0:
+                    cout << "Logging out...\n";
+                    break;
+                default:
+                    cout << "Invalid choice!\n";
+            }
+        } while(choice != 0);
+    }
+}; 
+
+class Customer : public User {
+	public: 
+		Customer() {
+			// constructor 
+		} 
+
+		~Customer() {
+			// destructor 
+		} 
+
+		void menu() {
+        int choice; 
+
+        do {
+            cout << "\n--- Customer Menu ---\n";
+            cout << "1. View Products\n";
+            cout << "2. Search Product\n";
+            cout << "0. Logout\n";
+            cout << "Enter choice: ";
+            cin >> choice;
+
+            switch(choice) {
+                case 1:
+                    cout << "View Products (coming soon)\n";
+                    break;
+                case 2:
+                    cout << "Search Product (coming soon)\n";
+                    break;
+                case 0:
+                    cout << "Logging out...\n";
+                    break;
+                default:
+                    cout << "Invalid choice!\n";
+            }
+        } while(choice != 0);
+    }
+}; 
+
+int main() { 
 	int choice; 
+	User* user = NULL; 
 
 	do {
 		cout << "\n===== Stationery Shop Management System =====\n"; 
@@ -16,10 +111,14 @@ int main() {
 
 		switch(choice) {
 			case 1:
-                cout << "Staff Module (coming soon)\n";
+                user = new Staff(); 
+				user->menu(); 
+				delete user; 
                 break;
             case 2:
-                cout << "Customer Module (coming soon)\n";
+                user = new Customer(); 
+				user->menu(); 
+				delete user;
                 break;
             case 0:
                 cout << "Exiting system...\n";
